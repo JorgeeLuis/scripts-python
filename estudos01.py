a536c4cfd717ce5b24bdd2ef0278627e8900f273
Jorge Luis <jlpontesfelipe@gmail.com>
2022-12-21 10:10:05 -0300

    ola

---
+++ estudos01.py
@@ -0,0 +1,8 @@
+nome=input(('digite seu nome: 'in ))
+idade=int(input(('qual é sua idade? ')))
+
+n1=idade
+n2=10
+s=n1+n2
+
+print('ola {}. Daqui a dez anos você vai ter {} anos'.format(nome,s))
---
+++ exercicios01.py
@@ -0,0 +1,10 @@
+nome = input(print('Qual é o seu nome? '))
+quantidade = input(print('qual é o valor adquerido? '))
+meta = input(print('qual é o valor da meta? '))
+if meta >= quantidade:
+    print('Parabéns {}, você bateu a meta de {}'.format(nome,quantidade))
+    print('Fim do programa!') 
+
+else:
+    print('{}, infelizmente não bateu a meta de {}'.format(nome,meta))
+    print('Fim do programa!')
