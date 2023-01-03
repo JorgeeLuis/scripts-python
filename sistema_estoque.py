e9d99e464f2cd6504177b6ba6bfc6c556f0b382d
Jorge Luis <jlpontesfelipe@gmail.com>
2023-01-03 00:05:02 -0300

    mini estoque

---
+++ sistema_estoque.py
@@ -0,0 +1,29 @@
+#mini sistema de estoque
+print('                         SISTEMA DE ESTOQUE                      v1.0')
+produto = input('nome do produto: ')
+categoria = input('categoria do produto: ')
+qtde_produtos = int (input('quantidade de produtos: '))
+
+#categorias:
+alimentos = 50
+bebidas = 75
+limpeza = 30
+
+if produto and categoria and qtde_produtos:
+    if categoria == 'bebidas':
+        if qtde_produtos <= bebidas:
+            print ('solicitar {}, á equipe de compras, temos apenas {} unidades em estoque. A quantidade minima é {}'.format(produto, qtde_produtos, bebidas))
+    elif categoria == limpeza:
+        if qtde_produtos <= limpeza:
+            print ('solicitar {}, á equipe de compras, temos apenas {} unidades em estoque. A quantidade minima é {}'.format(produto, qtde_produtos, limpeza))
+    elif categoria == alimentos:
+        if qtde_produtos <= alimentos:
+            print ('solicitar {}, á equipe de compras, temos apenas {} unidades em estoque. A quantidade minima é {}'.format(produto, qtde_produtos, alimentos))
+
+
+
+
+
+else:
+    print('preencha corretamente')        
+    
