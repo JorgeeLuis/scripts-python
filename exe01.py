83a3e51b40314e748d76fc7978fdd68f2f93551e
Jorge Luis <jlpontesfelipe@gmail.com>
2023-01-02 22:42:05 -0300

    curso_py

--- exe01.py
+++ exe01.py
@@ -1,10 +1,9 @@
 nome = input(print('Qual é o seu nome? '))
 quantidade = input(print('qual é o valor adquerido? '))
 meta = input(print('qual é o valor da meta? '))
-if meta >= quantidade:
+if quantidade >= meta:
     print('Parabéns {}, você bateu a meta de {}'.format(nome,quantidade))
     print('Fim do programa!') 
-
 else:
     print('{}, infelizmente não bateu a meta de {}'.format(nome,meta))
     print('Fim do programa!')
